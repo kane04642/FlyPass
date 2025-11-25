98e0e4dd096850c1753e7414b2f6c6c3d6528ec5
kane04642 <danieltorres1982@gmail.com>
2025-11-25 17:08:45 -0500

    update V 2

--- src/test/java/com/calidda/ov/runners/LoginRunner.java
+++ src/test/java/com/calidda/ov/runners/LoginRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        tags = "@Registrar",
+        tags = "@Login",
         features = "src/test/resources/features",
         glue = "com.calidda.ov"
 )
