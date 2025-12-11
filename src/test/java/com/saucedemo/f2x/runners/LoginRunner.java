ae0b2a377dcf0a65403037f28b49e1dfd4f6c922
kane04642 <danieltorres1982@gmail.com>
2025-12-10 21:41:27 -0500

    Agregar productos al carrito

--- src/test/java/com/saucedemo/f2x/runners/LoginRunner.java
+++ src/test/java/com/saucedemo/f2x/runners/LoginRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        tags = "@Login",
+        tags = "@AgregarProducto",
         features = "src/test/resources/features",
         glue = "com.saucedemo.f2x"
 )
---
+++ src/test/java/com/saucedemo/f2x/stepdefinitions/agregarProductos/AgregarProductoSteps.java
@@ -0,0 +1,20 @@
+package com.saucedemo.f2x.stepdefinitions.agregarProductos;
+
+import com.saucedemo.f2x.tasks.agregarProducto.AgregarProducto;
+import io.cucumber.java.es.Cuando;
+
+import java.util.Map;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
+public class AgregarProductoSteps {
+    @Cuando("el usuario selecciona el producto para el carro de compra")
+    public void elUsuarioSeleccionaElProductoParaElCarroDeCompra(Map<String, String> producto) {
+        String producto1 = producto.get("producto1");
+        String producto2 = producto.get("producto2");
+        theActorInTheSpotlight().wasAbleTo(
+                AgregarProducto.alCarrito(producto1),
+                AgregarProducto.alCarrito(producto2)
+        );
+    }
+}
